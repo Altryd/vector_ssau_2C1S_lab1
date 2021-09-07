a5ed2894543c8968d56db96e27896315626c5871
Altryd <michael_bor@mail.ru>
2021-09-07 18:47:33 +0400

    Added operators , some tests were made in main; alpha version i guess

--- Vector_lab/Vector.cpp
+++ Vector_lab/Vector.cpp
@@ -6,6 +6,33 @@ private:
 	size_t size;
 	size_t capacity;
 	size_t grow_size;
+	void ExpandCapacityByN(const size_t n, const size_t index = 0, const bool end_insertion = true)
+	{
+		size_t new_capacity = capacity + n;
+		double* new_data = new double[new_capacity];
+		if (end_insertion)
+		{
+			for (size_t i = 0; i < size; i++)
+			{
+				new_data[i] = data[i];
+			}
+		}
+		else
+		{
+			for (size_t i = 0; i < index; i++)
+			{
+				new_data[i] = data[i];
+			}
+			data[index] = 0;
+			for (size_t i = index + 1; i < size; i++)
+			{
+				new_data[i] = data[i - 1];
+			}
+		}
+		delete[] data;
+		data = new_data;
+		capacity = new_capacity;
+	}
 public:
 	Vector() : data(NULL), size(0), capacity(0), grow_size(10) {}
 	Vector(const size_t size)
@@ -39,4 +66,216 @@ public:
 	{
 		return size;
 	}
+	void PushBack(const double value)
+	{
+		if (size == 0 && capacity == 0 && data==NULL)
+		{
+			data = new double[grow_size];
+			data[size] = value;
+			size++;
+		}
+		else
+		{
+			if (size + 1 >= capacity) ExpandCapacityByN(grow_size);
+			data[size] = value;
+			size++;
+		}
+	}
+	void Insert(const double value, const size_t index)
+	{
+		if (index<0 || index>size) throw std::out_of_range("Index is out of range");
+		if (size == 0 && capacity ==0 && data ==NULL)
+		{
+			data = new double[grow_size];
+			data[size] = value;
+			size++;
+		}
+		else
+		{
+			if (size + 1 >= capacity) ExpandCapacityByN(grow_size);
+			for (size_t i = size + 1; i > index; --i)
+			{
+				std::swap(data[i], data[i - 1]);
+			}
+			data[index] = value;
+			size++;
+		}
+	}
+
+
+	void Erase(const size_t index)
+	{
+		if (size == 0) return;
+		if (index >= size) throw std::out_of_range("Index is out of range");
+		for (size_t i = index; i < size-1; ++i)
+		{
+			std::swap(data[i], data[i + 1]);
+		}
+		size--;
+	}
+	double& operator[] (const size_t index)
+	{
+		if (index >= size) throw std::out_of_range("Index is out of range");
+		return data[index];
+	}
+	double operator[](const size_t index) const
+	{
+		if (index >= size) throw std::out_of_range("Index is out of range");
+		return data[index];
+	}
+	Vector operator+= (const Vector& rhs)
+	{
+		if (size != rhs.size) throw std::exception("Bad dimensions");
+		for (size_t i = 0; i < size; i++)
+		{
+			data[i] += rhs.data[i];
+		}
+		return *this;
+	}
+	Vector operator+(const Vector& rhs) const
+	{
+		if (size != rhs.size) throw std::exception("Bad dimensions");
+		Vector result(*this);
+		result += rhs;
+		return result;
+	}
+	Vector operator-= (const Vector& rhs)
+	{
+		if (size != rhs.size) throw std::exception("Bad dimensions");
+		for (size_t i = 0; i < size; i++)
+		{
+			data[i] -= rhs.data[i];
+		}
+		return *this;
+	}
+	Vector operator-(const Vector& rhs) const
+	{
+		if (size != rhs.size) throw std::exception("Bad dimensions");
+		Vector result(*this);
+		result -= rhs;
+		return result;
+	}
+	double operator*(const Vector& rhs) const
+	{
+		if (size != rhs.size) throw std::exception("Bad dimensions");
+		double scalar_product = 0;
+		for (size_t i = 0; i < size; i++)
+		{
+			scalar_product += data[i] * rhs.data[i];
+		}
+		return scalar_product;
+	}
+	Vector operator*=(const double value)
+	{
+		for (size_t i = 0; i < size; i++)
+		{
+			data[i] *= value;
+		}
+		return *this;
+	}
+	Vector operator*(const double value) const
+	{
+		Vector result(*this);
+		result *= value;
+		return result;
+	}
+	Vector operator/=(const double value)
+	{
+		if (value == 0) throw std::exception("Dividing by zero is prohibited");
+		for (size_t i = 0; i < size; i++)
+		{
+			data[i] /= value;
+		}
+		return *this;
+	}
+	Vector operator/(const double value) const
+	{
+		Vector result(*this);
+		result /= value;
+		return result;
+	}
+	bool operator==(const Vector& rhs) const
+	{
+		if (size != rhs.size) return false;
+		for (size_t i = 0; i < size; i++)
+		{
+			if (data[i] != rhs.data[i]) return false;
+		}
+		return true;
+	}
+	bool operator!=(const Vector& rhs) const
+	{
+		return !(*this == rhs);
+	}
+	friend std::ostream& operator<< (std::ostream& out, const Vector& point);
 };
+std::ostream& operator<< (std::ostream& out, const Vector& vector)
+{
+	for (size_t i = 0; i < vector.size; i++)
+	{
+		out << vector.data[i] << " ";
+	}
+	out << std::endl;
+
+
+	return out;
+}
+Vector operator*=(const double value, Vector& rhs)
+{
+	rhs *= value;
+	return rhs;
+}
+Vector operator*(const double value, const Vector& rhs)
+{
+	Vector result(rhs);
+	result *= value;
+	return result;
+}
+
+int main()
+{
+	/*Vector a(4);
+	for (size_t i = 0; i < a.GetSize(); i++)
+	{
+		a[i] += 1 +double(i);
+	}
+	std::cout << a << std::endl;
+	Vector b(4);
+	for (size_t i = 0; i < b.GetSize(); i++)
+	{
+		b[i] += 1 + (double)i * (double)i +50;
+	}
+	std::cout << b << std::endl;
+	Vector c = a + b;
+	std::cout << c << std::endl;
+	double s_p = a * b;
+	std::cout << s_p << std::endl;*/
+	setlocale(LC_ALL, "Russian");
+	Vector data(0);
+	//a.Insert(5,0);
+	//a.PushBack(20);
+	for (size_t i = 0; i < 11; i++)
+	{
+		data.Insert((double)i + 1, i);
+	}
+	std::cout << "До:  ";
+	for (size_t i = 0; i < 11; i++)
+	{
+		std::cout << i << " ";
+	}
+	std::cout << std::endl;
+	std::cout << "До:  " << data << std::endl << std::endl;
+	size_t size = data.GetSize();
+	for (int i = size-2; i >= 0; i-=2)
+	{
+		try {
+			std::cout << data[i] << std::endl;
+			data.Erase((size_t)i);
+		}
+		catch (std::exception& err)
+		{
+			std::cout << err.what() << std::endl;
+		}
+	}
+	std::cout << "После:  " << data << std::endl;
+}
